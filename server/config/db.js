b1e3958cf9daf7b74ac018d0f07a4d4871942386
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-08 14:31:47 +0100

    :recycle: refactor: app.js foi refactorado, o mongodb já está funcional

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://kelvinfabio2:YsemPrXYtyDksMrn@cluster0.ngr4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = mongodb+srv://kelvinfabio2:YsemPrXYtyDksMrn@cluster0.ngr4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;
+
+require('dotenv').config();
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 async function run() {
   try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    
+    await mongoose.connect(process.env.MONGODB_URI, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    
     await mongoose.disconnect();
   }
 }
 run().catch(console.dir);
+
+run()
+
+module.exports = mongoose
