b023508045be822f8159c4db08b693518adcbfe7
Fabio Kelvin <kelvinfabio2@gmail.com>
2025-02-08 16:00:21 +0100

    action.js

--- server/routes/action.js
+++ server/routes/action.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const { createAction, getActions, deleteAction, updateAction } = require('../controllers/actionController');
 
-const router = express.Router();
-
-router.use(authMiddleware);
-
-router.post('/actions', createAction);
-router.get('/actions', getActions);
-router.delete('/actions',deleteAction);
-router.put('/actions',updateAction)
+router.post('/', auth, createAction);
+router.get('/', auth, getActions);
+router.put('/:id', auth, updateAction);
+router.delete('/:id', auth, deleteAction);
 
 module.exports = router;
