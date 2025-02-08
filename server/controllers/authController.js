3717bfaedd3c062a1d627fa7273f3f1c9e912bda
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-08 12:34:59 +0100

    :lipstick: feat: O controller feito com os metodos registar e login

---
+++ server/controllers/authController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User')
+const JWT = require('jsonwebtoken')
+
+const register = async (req, res) => {
+
+    const { username, password } = req.body;
+
+    try {
+
+        const user = new User( { username, password} )
+        await user.save()
+
+        res.status(201).json( 
+            { message: 'Usuário registrado com sucesso!' }
+        )
+
+    } catch ( err ) {
+        res.status(400)
+            .json( { error: err.message} )
+    }
+}
+
+const login = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+
+        const user = await User.findOne( ( username ) )
+
+        if ( user || !(await bcrypt.compare(password, user.password)) ) {
+            return res.status(400)
+                .json( { error: 'Credenciais inválidas' } )
+        }
+
+        const token = JWT.sign( { userId: user._id}, 
+            process.env.JWT_SECRET, { expiresIn: '1h' }
+        )
+        
+        res.json({ token })
+    } catch (err) {
+        res.status(400)
+            .json( { error: err.message } )
+    }
+}
+
+module.exports = { register, login }
